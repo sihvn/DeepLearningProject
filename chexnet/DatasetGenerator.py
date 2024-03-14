151cc0e9a08a7e5b18bc3ef5151604c01bea4af0
wangsiyang68 <siyang.official@gmail.com>
2024-03-14 23:25:13 +0800

    feat: first working model from zoogzog
    https://github.com/zoogzog/chexnet

---
+++ chexnet/DatasetGenerator.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+#-------------------------------------------------------------------------------- 
+
+class DatasetGenerator (Dataset):
+    
+    #-------------------------------------------------------------------------------- 
+    
+    def __init__ (self, pathImageDirectory, pathDatasetFile, transform):
+    
+        self.listImagePaths = []
+        self.listImageLabels = []
+        self.transform = transform
+    
+        #---- Open file, get image paths and labels
+    
+        fileDescriptor = open(pathDatasetFile, "r")
+        
+        #---- get into the loop
+        line = True
+        # limit = 100
+        # i = 0
+        # while line and i < limit:
+        while line:
+                
+            line = fileDescriptor.readline()
+            
+            #--- if not empty
+            if line:
+          
+                lineItems = line.split()
+                
+                imagePath = os.path.join(pathImageDirectory, lineItems[0])
+                imageLabel = lineItems[1:]
+                imageLabel = [int(i) for i in imageLabel]
+                
+                self.listImagePaths.append(imagePath)
+                self.listImageLabels.append(imageLabel)   
+
+            # i += 1
+            
+        fileDescriptor.close()
+
+        # print(f"Collected {i} images from {pathDatasetFile}")
+    #-------------------------------------------------------------------------------- 
+    
+    def __getitem__(self, index):
+        
+        imagePath = self.listImagePaths[index]
+        
+        imageData = Image.open(imagePath).convert('RGB')
+        imageLabel= torch.FloatTensor(self.listImageLabels[index])
+        
+        if self.transform != None: imageData = self.transform(imageData)
+        
+        return imageData, imageLabel
+        
+    #-------------------------------------------------------------------------------- 
+    
+    def __len__(self):
+        
+        return len(self.listImagePaths)
+    
+ #-------------------------------------------------------------------------------- 
+    
