877df29223afe7ab187275ba06aa4c416b8afb37
wangsiyang68 <siyang.official@gmail.com>
2024-04-16 11:16:23 +0800

    feat: add colour and conf threshold

--- deployment/streamlit.py
+++ deployment/streamlit.py
@@ -13,13 +13,13 @@ from custom_densenet import *
 from preprocessing import *
 
 ## get predictions for this batch
-def process_data(model, batch):
+def process_data(model, batch, confidence):
     ## pass data through model
     images, labels = batch
     images = images.to(device)
     with torch.no_grad():
         outputs = model(images)
-    pred_labels = (outputs > 5/10).long().T.squeeze(0)
+    pred_labels = (outputs > confidence).long().T.squeeze(0)
 
     return images, pred_labels, labels
 
@@ -44,8 +44,12 @@ def display_batch(images, pred_labels, labels):
         if j % 4 == 0:
             cols = st.columns(4)
         image_numpy = np.transpose(image.cpu().detach().numpy(), (1, 2, 0))  # Convert tensor to numpy array and transpose dimensions
-        cols[j%4].image(image_numpy, clamp = True, caption=f"Pred: {pred_class.item()}, Actual: {label.item()}", use_column_width=True)
-
+        if pred_class.item() == label.item():
+            caption = f":green[Pred: {pred_class.item()}, Actual: {label.item()}]"
+        else: 
+            caption = f":red[Pred: {pred_class.item()}, Actual: {label.item()}]"
+        cols[j%4].image(image_numpy, clamp = True, use_column_width=True)
+        cols[j%4].caption(caption)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 ## import model
@@ -70,24 +74,37 @@ def get_iterator():
     batch_iterator = iter(test_loader)
     return batch_iterator
 
+@st.cache_resource
+def get_batch_data(_batch_iterator):
+    return next(_batch_iterator)
+
 st.title('Predict Pneumothorax of Test Dataset Batch')
 
 ## setup data from iterator and model
 batch_iterator = get_iterator()
-batch = next(batch_iterator)
+
+## to start the batch initially 
+if "batch" not in st.session_state:
+    batch = get_batch_data(batch_iterator)
+    st.session_state["batch"] = batch
+
+batch = st.session_state["batch"]
 model = load_model("some path to be defined")
+
+conf = st.slider('Confidence Threshold', min_value=0.0, max_value=1.0, value=0.5, step=0.1) 
 
 # Add a button to iterate through the dataloader
 if st.button('Next Batch'):
     try:
         batch = next(batch_iterator)
+        st.session_state["batch"] = batch
         
     except StopIteration:
         st.write("No more batches available.")
 
 ## setup streamlit data
-images, pred_labels, labels = process_data(model, batch)
+images, pred_labels, labels = process_data(model, batch, conf)
 tp_count, fp_count, fn_count, tn_count = calculate_stats(pred_labels, labels)
-st.write(f"True Positives: {tp_count}, True Negatives: {tn_count}")
-st.write(f"False Positives: {fp_count}, False Negatives: {fn_count}")
+st.write(f":green[True Positives: {tp_count}, True Negatives: {tn_count}]")
+st.write(f":red[False Positives: {fp_count}, False Negatives: {fn_count}]")
 display_batch(images, pred_labels, labels)
